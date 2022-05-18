5f8fe99a7b529543d31f0c202063d6498c56e701
AZ <a.zaprudnow@gmail.com>
2022-05-18 09:49:39 +0300

    Descriptions and comments to functions in main.py

--- MountainPass/MountainPass/main.py
+++ MountainPass/MountainPass/main.py
@@ -31,6 +31,7 @@ def get_db():
 async def unicorn_exception_handler(request: Request, exc: ErrorCreatingRecord):
     """
     Декорирование ошибки создания записи в БД
+    (развернутое описание ошибки вместо Internal Server Error).
     :param request: запрос
     :param exc: переопределен класс Exception
     :return: результат ошибки в формате JSON
@@ -42,6 +43,7 @@ async def unicorn_exception_handler(request: Request, exc: ErrorCreatingRecord):
 async def srv_connect_exception_handler(request: Request, exc: ErrorConnectionServer):
     """
     Декорирование ошибки проверки подключения к БД
+    (развернутое описание ошибки вместо Internal Server Error).
     :param request: запрос
     :param exc: переопределен класс Exception
     :return: результат ошибки в формате JSON
@@ -56,6 +58,7 @@ async def validation_exception_handler(
 ):
     """
     Декорирование ошибки на несоответствия схеме объекта БД
+    (развернутое описание ошибки вместо Request Validation Error).
     :param request: запрос
     :param exc: класс исключения RequestValidationError
     :return: результат ошибки в формате JSON с дополнением описанием и схемой
@@ -70,6 +73,14 @@ async def validation_exception_handler(
 
 @app.post("/users/", response_model=schemas.User)
 async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    """
+    Вызов функции создание пользователя и предварительная проверка наличия в БД по email,
+    если пользователь с email существует, вызов исключения.
+    Используется декоратор POST.
+    :param user: класс базовой модели пользователя
+    :param db: сессия подключения к БД
+    :return: сообщение в формате JSON о результате создания и id объекта
+    """
     db_user = crud.get_user_by_email(db, email=user.email)
 
     if db_user:
@@ -81,29 +92,54 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 @app.get("/users/", response_model=List[schemas.User])
 async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """
+    Получение id пользователей
+    Используется декоратор GET.
+    :param skip: пропуск по id
+    :param limit: лимит выборки по количеству записей
+    :param db: сессия подключения к БД
+    :return: сообщение в формате JSON (id объектов)
+    """
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
 @app.post("/coords/", response_model=schemas.Coords)
 async def create_coords(coords: schemas.CoordsCreate, db: Session = Depends(get_db)):
+    """
+    Вызов функции создание пользователя и предварительная проверка наличия в БД по email,
+    если пользователь с email существует, вызов исключения.
+    Используется декоратор POST.
+    :param coords: класс базовой модели координат
+    :param db: сессия подключения к БД
+    :return: сообщение в формате JSON id объекта
+    """
     return crud.create_coords(db=db, coords=coords)
 
 
 @app.post("/submitData/", response_model=schemas.PerevalAdded)
 async def submitData(pereval: schemas.PerevalAddedCreate, db: Session = Depends(get_db)):
-
+    """
+    Вызов функции создания пользователя, координат и записи о перевале,
+    Используется декоратор POST.
+    :param pereval: класс базовой модели перевала
+    :param db: сессия подключения к БД
+    :return: в случае успешного создания записи в БД, возвращается JSON с ответом
+    об успешной отправке и id объекта (перевала)
+    """
+    # Предварительная проверка подключения (простое выполнение запроса)
     try:
         db.execute('SELECT * FROM users')
     except Exception as error:
         raise ErrorConnectionServer(f'Ошибка соединения с сервером: {error}')
-
+    # Создание записи пользователя
     new_user = crud.create_user(db=db, user=pereval.user)
+    # Создание записи координат
     new_coords = crud.create_coords(db=db, coords=pereval.coords)
 
     pereval.user = new_user
     pereval.coords = new_coords
-
+    # Создание записи о перевале
     new_pereval = crud.create_pereval(db=db, pereval=pereval)
 
     return get_json_response(200, 'Отправлено успешно', new_pereval.id)

dc7a479ca0f4f72efe2899f5b5474907c316dfff
AZ <a.zaprudnow@gmail.com>
2022-05-19 21:47:34 +0300

    Description of functions and classes (functions); SECRET_KEY to .env

--- MountainPass/MountainPass/functions.py
+++ MountainPass/MountainPass/functions.py
@@ -2,7 +2,11 @@ from dotenv import dotenv_values
 
 
 def get_env(name_const=None):
-
+    """
+    Функция для получения переменных окружения из .env.
+    :param name_const: имя переменной (константы)
+    :return: значение или None
+    """
     for el_list in dotenv_values(".env").items():
         if str(el_list[0]) == str(name_const):
             return el_list[1]
--- MountainPass/MountainPass/settings.py
+++ MountainPass/MountainPass/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)$g-v)4xf!vj7z_dpga7jv(3s6mn=!4ts#k@i1sp1fh^r0h)j='
+SECRET_KEY = get_env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
